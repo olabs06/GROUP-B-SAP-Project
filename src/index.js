a4b2985f0b6a8c86694a9f85bce3659e5275c308
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-29 04:18:30 +0100

    Search logic implemented alongside base initial design

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+const initialize = () => {
+    search = document.querySelector('#searchID')
+    form = document.querySelector('#searchFormID')
+    body = document.querySelector('body')
+    p = document.querySelector('p')
+    refresh = document.querySelector('#refresh')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        fetch('https://techcrunch.com/wp-json/wp/v2/posts?per_page=100&context=embed')
+        .then(response => response.json())
+        .then((data) => {
+            refresh.innerHTML = ''
+            data.forEach(datum => {
+                title = document.createElement('h3')
+                titleDiv = document.createElement('div')
+                refresh.append(titleDiv)
+                titleDiv.append(title)
+                const array = datum["title"].rendered.split(' ')
+                array.filter(item => {
+                    if (item.toUpperCase() === search.value.toUpperCase()){
+                        return  title.innerHTML = array.join(' ')
+                    }})        
+            });
+        })
+        .catch(e => console.log(e.message))
+
+    })
+}
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Document Loaded")
+    initialize()
 })
