fc7e4dda326c9df327d09f16359dc414eb393c7b
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-06-29 13:19:08 +0100

    feed logic implemented alongside initial design

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 const initialize = () => {
-    search = document.querySelector('#searchID')
-    form = document.querySelector('#searchFormID')
-    body = document.querySelector('body')
-    p = document.querySelector('p')
-    refresh = document.querySelector('#refresh')
+    fetch('https://techcrunch.com/wp-json/wp/v2/posts?per_page=100&context=embed')
+    .then(response => response.json())
+    .then((data) => {
+        data.forEach((datum) => {
+            feedTitle = document.createElement('h2')
+            preview = document.createElement('h4')
+            feed = document.createElement('div')
+            feedText = document.createElement('div')
+            img = document.createElement('img')
+            a = document.createElement('a')
+
+            container.append(feed)
+            a.append(feedTitle)
+            feedText.append(a,preview)
+            feed.append(img,feedText)
+
+            img.className = 'feedImg'
+            feed.className = 'feed'
+            feedText.className = 'feedText'
+            img.src = datum["jetpack_featured_media_url"]
+            feedTitle.innerHTML = datum["title"].rendered
+            a.href = datum["link"]
+            preview.innerHTML = datum["excerpt"].rendered
+
+        })
+    })
+    .catch(e => console.log(e.message))
+
 
     form.addEventListener('submit', (e) => {
         e.preventDefault()
         fetch('https://techcrunch.com/wp-json/wp/v2/posts?per_page=100&context=embed')
         .then(response => response.json())
         .then((data) => {
-            refresh.innerHTML = ''
+            container.innerHTML = ''
             data.forEach(datum => {
                 title = document.createElement('h3')
                 titleDiv = document.createElement('div')
-                refresh.append(titleDiv)
+                container.append(titleDiv)
                 titleDiv.append(title)
                 const array = datum["title"].rendered.split(' ')
                 array.filter(item => {
@@ -24,15 +47,16 @@ const initialize = () => {
             });
         })
         .catch(e => console.log(e.message))
-
     })
+
 }
 
 
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
+    container = document.querySelector('#container')
+    search = document.querySelector('#searchID')
+    form = document.querySelector('#searchFormID')
+    p = document.querySelector('p')
     console.log("Document Loaded")
     initialize()
 })
